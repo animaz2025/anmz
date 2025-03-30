31a3f5b66c82d5a5ffb0a32a26357451ae84d0e1
animaz2025 <rustemefo12@gmail.com>
2025-03-30 16:30:29 +0400

    commit_message

--- vacib/script.js
+++
@@ -1,7 +0,0 @@
-function searchFilm(){let input=document.getElementById('search').value.toLowerCase();let films=document.querySelectorAll('.film');films.forEach(function(film){let title=film.querySelector('p').textContent.toLowerCase();if(title.includes(input)){film.style.display="block"}else{film.style.display="none"}})}
-function filterByCategory(){let selectedCategory=document.getElementById('categorySelect').value;let films=document.querySelectorAll('.film');films.forEach(function(film){if(selectedCategory===""||film.getAttribute('data-category')===selectedCategory){film.style.display="block"}else{film.style.display="none"}})}
-function applyFilters(){let input=document.getElementById('search').value.toLowerCase();let selectedCategory=document.getElementById('categorySelect').value;let films=document.querySelectorAll('.film');films.forEach(function(film){let title=film.querySelector('p').textContent.toLowerCase();let filmCategory=film.getAttribute('data-category');if((selectedCategory===""||filmCategory===selectedCategory)&&title.includes(input)){film.style.display="block"}else{film.style.display="none"}})}
-window.addEventListener("load", function() {
-    document.getElementById("preloader").classList.add("hidden");
-});
-
